3968f7d4d700ceca2856e3bf53e79eccd7a93f02
EDILSO TEODORO DE AMORIM <edylso@gmail.com>
2025-08-27 15:57:05 -0300

    Update script.bat

--- k8s-projeto1-app-base-main/script.bat
+++ k8s-projeto1-app-base-main/script.bat
@@ -7,3 +7,11 @@ echo "Realizando o push das imagens....."
 
 docker push edilso/projeto-backend:1.0
 docker push edilso/projeto-database:1.0
+
+echo "Criando os serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments no cluster kubernetes..."
+
+kubectl apply -f ./deployment.yml
